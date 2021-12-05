d026daffb44d57bb2e5a80e3c72ac1795c77566d
Jean-Yves Guyomarc'h <jean-yves.guyomarc-h@sap.com>
2021-12-05 11:51:11 -0800

    d5 read input to tuple list

--- d5_hydro_vents/hydro_vents.py
+++ d5_hydro_vents/hydro_vents.py
@@ -1,8 +1,16 @@
 #--- Day 5: Hydrothermal Venture ---
 #https://adventofcode.com/2021/day/5
 
+import re
+
 # Get data
 f = open('d5_hydro_vents/vents_input_test.txt','r')
 lines = f.read().splitlines()
 
-print(len(lines))
+vents_tuple = []
+for line in lines:
+    re_match = re.search(r"(\d+),(\d+) -> (\d+),(\d+)", line)
+    x1, y1, x2, y2 = re_match.groups()
+    vents_tuple.append([(int(x1), int(y1)), (int(x2), (int(y2)))])
+
+print(vents_tuple)
