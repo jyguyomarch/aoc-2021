0207d8c14c97cf239886e18625d10d48e18a8855
Jean-Yves Guyomarc'h <jean-yves.guyomarc-h@sap.com>
2021-12-05 11:20:55 -0800

    day 5 initial commit

---
+++ d5_hydro_vents/hydro_vents.py
@@ -0,0 +1,8 @@
+#--- Day 5: Hydrothermal Venture ---
+#https://adventofcode.com/2021/day/5
+
+# Get data
+f = open('d5_hydro_vents/vents_input_test.txt','r')
+lines = f.read().splitlines()
+
+print(len(lines))
