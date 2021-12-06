15a36ba5dd4597987ff383316c3610eef09593fe
Jean-Yves Guyomarc'h <jean-yves.guyomarc-h@sap.com>
2021-12-05 17:37:08 -0800

    d1: added test input

--- d1_sonar_sweep/sonar.py
+++ d1_sonar_sweep/sonar.py
@@ -3,10 +3,9 @@
 
 f = open('d1_sonar_sweep/input.txt','r')
 lines = f.readlines()
-#depth = list(map(int, lines))
 depth_readings = [int(depth) for depth in lines]
 
-#PART 1: compare sliding window of 2 depth
+#PART 1: compare sliding window of depth 2
 depth_pair = zip(depth_readings, depth_readings[1:])
 depth_increase = 0
 for depth1, depth2 in depth_pair:
@@ -14,7 +13,7 @@ for depth1, depth2 in depth_pair:
         depth_increase += 1
 print('AOC Day 1 - Sonar Sweep - Part 1: ' + str(depth_increase))
 
-#PART 2: compare sliding window of 3 depth
+#PART 2: compare sliding window of depth 3
 depth_trio = zip(depth_readings, depth_readings[1:], depth_readings[2:])
 depth_trio_sums = []
 for trio in depth_trio:
