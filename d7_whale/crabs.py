2c60140807e4632feb125ff0ca0f67a80f0ff863
Jean-Yves Guyomarc'h <jy.guyomarch@gmail.com>
2021-12-07 10:49:46 -0800

    D7: used median for part 1, added a quick exit for part 2, and the index

--- d7_whale/crabs.py
+++ d7_whale/crabs.py
@@ -1,26 +1,32 @@
 #--- Day 7: The Treachery of Whales ---
 #https://adventofcode.com/2021/day/7
 
+import statistics
+
 # Get Data
-f = open('d7_whale/crab_input.txt','r')
+f = open('d7_whale/crab_input_test.txt','r')
 crabs = [int(pos) for pos in f.read().split(',')]
 
-min_fuel = float('inf')
-min_fuel_tri = float('inf')
+# Part 1: use the median
+med = statistics.median(crabs)
+min_fuel = sum([abs(crab_position - med) for crab_position in crabs])
+print(f'Part 1: Crab position and fuel: {med}, {min_fuel}')
 
-# brute force going through all possibilities
+# Part 2: brute force going through all possibilities with quick exit
+min_fuel_tri = float('inf')
+best_position = float('inf')
 for i in range(min(crabs), max(crabs)):
-    fuel = 0
     fuel_tri = 0
     for crab in crabs:
         n = abs(crab - i)
-        fuel += n
         fuel_tri += n*(n+1)/2
-    min_fuel = min(min_fuel, fuel)
-    min_fuel_tri = min(min_fuel_tri, fuel_tri)
+        if fuel_tri > min_fuel:
+            exit
+    if fuel_tri < min_fuel_tri:
+        min_fuel_tri = fuel_tri
+        best_position = i
 
-print(min_fuel)
-print(min_fuel_tri)
+print(f'Part 2: Crab position and fuel: {best_position}, {min_fuel_tri}')
 
 # Part 1: 347509
 # Part 2: 98257206
