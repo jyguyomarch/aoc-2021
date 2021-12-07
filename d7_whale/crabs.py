c0b638d413d52d2cca7e7cf981a4703a2bc0e55c
Jean-Yves Guyomarc'h <jy.guyomarch@gmail.com>
2021-12-06 21:43:45 -0800

    D7: solved

--- d7_whale/crabs.py
+++ d7_whale/crabs.py
@@ -2,7 +2,27 @@
 #https://adventofcode.com/2021/day/7
 
 # Get Data
-f = open('d7_whale/crab_input_test.txt','r')
+f = open('d7_whale/crab_input.txt','r')
 crabs = [int(pos) for pos in f.read().split(',')]
 
-print(crabs)
+min_fuel = None
+min_fuel_tri = None
+
+# brute force going through all possibilities
+for i in range(max(crabs)):
+    fuel = 0
+    fuel_tri = 0
+    for crab in crabs:
+        n = abs(crab - i)
+        fuel += n
+        fuel_tri += n*(n+1)/2
+    if min_fuel is None or fuel < min_fuel:
+        min_fuel = fuel
+    if min_fuel_tri is None or fuel_tri <  min_fuel_tri:
+        min_fuel_tri = fuel_tri
+
+print(min_fuel)
+print(min_fuel_tri)
+
+# Part 1: 347509
+# Part 2: 98257206
