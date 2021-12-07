697503e2b061ea554845afe277e3f0197595e3d7
Jean-Yves Guyomarc'h <jy.guyomarch@gmail.com>
2021-12-06 21:06:41 -0800

    D7: init puzzle and get data

---
+++ d7_whale/crabs.py
@@ -0,0 +1,8 @@
+#--- Day 6: Lanternfish ---
+#https://adventofcode.com/2021/day/7
+
+# Get Data
+f = open('d7_whale/crab_input_test.txt','r')
+crabs = [int(pos) for pos in f.read().split(',')]
+
+print(crabs)
