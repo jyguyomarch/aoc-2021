cd1c947fd82fc7b4bc38172d3d8040d6f8822a84
Jean-Yves Guyomarc'h <jy.guyomarch@gmail.com>
2021-12-06 17:18:23 -0800

    d6 clean-up

--- d6_lanternfish/lanternfish.py
+++ d6_lanternfish/lanternfish.py
@@ -5,28 +5,18 @@
 f = open('d6_lanternfish/fish_input.txt','r')
 lanternfishs = [int(fish) for fish in f.read().split(',')]
 
-# for i in range(0):
-#     new_fishies = 0
-#     for j in range(len(lanternfishs)):
-#         if lanternfishs[j] == 0:
-#             lanternfishs[j] = 6
-#             new_fishies += 1
-#         else:
-#             lanternfishs[j] -= 1
-#     lanternfishs += [ 8 for x in range(new_fishies)]
-
-# print(f'Lanternfish pop: {len(lanternfishs)}')
-
+# Create and init fish counter
 fish_counter = [ 0 for x in range(9)]
 for x in lanternfishs:
     fish_counter[int(x)] += 1
 
-for tick in range(80):
+# Process life
+for tick in range(256):
     for i in range(8):
         if i == 0:
             new_fishies = fish_counter[i]
         fish_counter[i] = fish_counter[i+1]
-    # new babies
+    # add new bor fishies
     fish_counter[8] = new_fishies
     # reset 0 fish to 6
     fish_counter[6] += new_fishies
