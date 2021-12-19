5c27ca50f86aaebd0adc11162a897b95a1511df2
Jean-Yves Guyomarc'h <jy.guyomarch@gmail.com>
2021-12-19 12:48:15 -0800

    D18: defined magnitude and tests + addition of all snail numbers

--- d18_snailfish/snailfish.py
+++ d18_snailfish/snailfish.py
@@ -13,11 +13,24 @@ def snail_explode(snail_number):
 def snail_split(snail_number):
     pass
 
+def magnitude(snail_number):
+    if type(snail_number) == int:
+        return snail_number
+    return 3 * magnitude(snail_number[0]) + 2 * magnitude(snail_number[1])
+
 f = open('d18_snailfish/snailfish_input_test.txt','r')
 lines = f.read().splitlines()
 
 snail_number_list = []
-for line in lines:
-    snail_number = eval(line)
-    print(snail_number)
-    snail_number_list.append(snail_number)
+added_snail = []
+for i in range(len(lines) - 1):
+    if len(added_snail) == 0:
+        added_snail = eval(lines[i])
+    added_snail = snail_add(added_snail, eval(lines[i+1])) 
+
+# test magnitude
+assert magnitude([[1,2],[[3,4],5]]) == 143
+assert magnitude([[[[0,7],4],[[7,8],[6,0]]],[8,1]]) == 1384
+assert magnitude([[[[1,1],[2,2]],[3,3]],[4,4]]) == 445
+assert magnitude([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]) == 3488
+    
